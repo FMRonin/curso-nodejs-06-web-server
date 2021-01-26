2d7556063693b36c66b127bb4d3f3a89da8beede
Fabian Martinez <fabianemartinez@gmail.com>
2021-01-25 21:34:17 -0500

    fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ hbs.registerPartials(__dirname + '/views/partials', function (err) {})
 //Helpers
 hbs.registerHelper('anio', (options) => new Date().getFullYear());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
     res.render('home',{
